11961dd02ab9846c14c17384eddd18064bd7529c
LeeJaeWon <ghkdwpqndtls@naver.com>
2022-05-15 02:01:45 +0900

    - jCenter replaced with mavenCentral. - Add convertGoalTime function.

--- app/src/main/java/com/jeepchief/clubhouse/view/matchrecord/adapter/MatchDetailAdapter.kt
+++ app/src/main/java/com/jeepchief/clubhouse/view/matchrecord/adapter/MatchDetailAdapter.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import java.lang.StringBuilder
+import kotlin.math.pow
 
 class MatchDetailAdapter(private val goalInfoList: List<ShootDetailBean>) : RecyclerView.Adapter<MatchDetailAdapter.MatchDetailViewHolder>() {
     private lateinit var context: Context
@@ -32,10 +33,11 @@ class MatchDetailAdapter(private val goalInfoList: List<ShootDetailBean>) : Recy
     override fun onBindViewHolder(holder: MatchDetailViewHolder, position: Int) {
         holder.apply {
 //            getGoalInfo(shootDetailBean).also { Log.e("list size is $it") }
+//            goalInfoList.sortedByDescending { it.goalTime }
             CoroutineScope(Dispatchers.Main).launch {
                 goalInfoList[position].run {
                     tvPlayerName.text = StringBuilder("득점 : ").append(getGoalPlayer(spId.toString()))
-                    tvGoalTime.text = (goalTime).toString().plus("분")
+                    tvGoalTime.text = convertGoalTime(goalTime).toString().plus("분")
                     tvAssistNam.text = if(assistSpId == -1) "No Assist" else StringBuilder("도움 : ").append(getAssistPlayer(assistSpId.toString()))
                 }
             }
@@ -72,5 +74,25 @@ class MatchDetailAdapter(private val goalInfoList: List<ShootDetailBean>) : Recy
         return deferred.await()
     }
 
-    private fun convertSecond(time: Int) : Int = time / 60
+    private fun convertGoalTime(time: Int) : Int {
+        val rangeBase = 2.0.pow(24.0).toInt()
+        return when(time) {
+            in rangeBase * 0 until rangeBase * 1 -> { // 전반전
+                time / 60
+            }
+            in rangeBase * 1 until rangeBase * 2 -> { // 후반전, rangeBase * 1 차감 후 45 * 60s 더하기
+                (time - (rangeBase * 1) + (45 * 60)) / 60
+            }
+            in rangeBase * 2 until rangeBase * 3 -> { // 연장 전반전, rangeBase * 2 차감 후 90 * 60s 더하기
+                (time - (rangeBase * 2) + (90 * 60)) / 60
+            }
+            in rangeBase * 3 until rangeBase * 4 -> { // 연장 후반전, rangeBase * 3 차감 후 105 * 60s 더하기
+                (time - (rangeBase * 3) + (105 * 60)) / 60
+            }
+            in rangeBase * 4 until rangeBase * 5 -> { // 승부차기, rangeBase * 4 차감 후 120 * 60s 더하기
+                (time - (rangeBase * 4) + (120 * 60)) / 60
+            }
+            else -> { 0 }
+        }
+    }
 }
